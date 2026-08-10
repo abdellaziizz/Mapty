f13bdbb724a9761eef0129dba2a6414b50a0a533
ahmed abdelaziz <a.abdelaziz2338@gmail.com>
2026-08-10 15:32:14 +0300

    adding leaflet functin

--- script.js
+++ script.js
@@ -12,7 +12,21 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 navigator.geolocation.getCurrentPosition(
   function (p) {
-    console.log(p);
+    const { latitude } = p.coords;
+    const { longitude } = p.coords;
+    const coords = [latitude, longitude];
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    const map = L.map('map').setView(coords, 13); //13 is zooming in and out
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+      .openPopup();
   },
   function () {
     console.log('cannot find your position');
